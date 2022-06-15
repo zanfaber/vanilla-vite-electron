1ee95a72f88f6ff09299237043ac82eff25db8cc
ZanFaber <870384+zanfaber@users.noreply.github.com>
2022-06-15 21:16:42 +0200

    feat: add necessary files for electron conf
    Also sets config for vite

---
+++ preload.js
@@ -0,0 +1,16 @@
+const {
+    ipcRenderer,
+    contextBridge
+} = require("electron");
+
+// Expose protected methods off of window (ie.
+// window.api.sendToA) in order to use ipcRenderer
+// without exposing the entire object
+contextBridge.exposeInMainWorld("api", {
+    sendToA: function(){
+        ipcRenderer.send("A");
+    },
+    receiveFromD: function(func){
+        ipcRenderer.on("D", (event, ...args) => func(event, ...args));
+    }
+});
